b42c5a32fe145ffd8d21144b93c038fce5614d56
OleksiiFltv <filatov26@ukr.net>
2024-07-17 17:30:01 +0300

    homework20 - 3

---
+++ cypress/support/carModels.js
@@ -0,0 +1,37 @@
+const carModels = {
+  Audi: {
+    1: { carBrandId: 1, title: "TT" },
+    2: { carBrandId: 1, title: "R8" },
+    3: { carBrandId: 1, title: "Q7" },
+    4: { carBrandId: 1, title: "A6" },
+    5: { carBrandId: 1, title: "A8" },
+  },
+  BMW: {
+    6: { carBrandId: 2, title: "3" },
+    7: { carBrandId: 2, title: "5" },
+    8: { carBrandId: 2, title: "X5" },
+    9: { carBrandId: 2, title: "X6" },
+    10: { carBrandId: 2, title: "Z3" },
+  },
+  Ford: {
+    11: { carBrandId: 3, title: "Fiesta" },
+    12: { carBrandId: 3, title: "Focus" },
+    13: { carBrandId: 3, title: "Fusion" },
+    14: { carBrandId: 3, title: "Mondeo" },
+    15: { carBrandId: 3, title: "Sierra" },
+  },
+  Porsche: {
+    16: { carBrandId: 4, title: "911" },
+    17: { carBrandId: 4, title: "Cayenne" },
+    18: { carBrandId: 4, title: "Panamera" },
+  },
+  Fiat: {
+    19: { carBrandId: 5, title: "Palio" },
+    20: { carBrandId: 5, title: "Ducato" },
+    21: { carBrandId: 5, title: "Panda" },
+    22: { carBrandId: 5, title: "Punto" },
+    23: { carBrandId: 5, title: "Scudo" },
+  },
+};
+
+export default carModels;
